69e992b62a83681b9cca8c937f8e8b0b6e47ce67
koturn <jeak.koutan.apple@gmail.com>
2020-11-30 17:15:43 +0900

    Enable to create zip file if possible

--- MoveFileByTimestamp/Program.cs
+++ MoveFileByTimestamp/Program.cs
@@ -1,19 +1,33 @@
 ﻿using System;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
-
+using System.Threading;
 
 namespace MoveFileByTimestamp
 {
-    class Program
+    public class Program
     {
+        /// <summary>
+        /// PNG、JPEGはzipによってほとんど圧縮出来ないため、合計サイズが2GB以内でなければ
+        /// zip圧縮の対象としない。
+        /// </summary>
+        private const long MaxSumFileSize = 2L * 1024L * 1024L * 1024L;
+        /// <summary>
+        /// <para>デフォルトの時刻オフセット</para>
+        /// <para>この時間分を前日に含める</para>
+        /// <para>デフォルトでは05:00:00なので、2020/11/01 05:00:00 ~ 2020/11/02 04:59:59 を 2020/11/01 として扱う</para>
+        /// </summary>
         private static readonly TimeSpan DefaultOffsetTs;
+        /// <summary>
+        /// デフォルトの対象ファイル拡張子
+        /// </summary>
         private static string[] DefaultExts;
 
         static Program()
         {
             DefaultOffsetTs = new TimeSpan(5, 0, 0);
-            DefaultExts = new[] { ".png", ".jpg", "snip" };
+            DefaultExts = new[] { ".png", ".jpg", ".snip" };
         }
 
         static void Main(string[] args)
@@ -23,7 +37,7 @@ namespace MoveFileByTimestamp
 
             Directory.EnumerateFiles(".")
                 .AsParallel()
-                .Where(fileName => exts.Any(ext => fileName.EndsWith(ext)))
+                .Where(fileName => exts.Any(ext => fileName.EndsWith(ext, StringComparison.OrdinalIgnoreCase)))
                 .Select(fileName => (FileName: fileName, LastWriteTime: File.GetLastWriteTime(fileName)))
                 .Select(tpl => (tpl.FileName, tpl.LastWriteTime, OffsetLastWriteTime: (tpl.LastWriteTime - ot).Date))
                 .GroupBy(tpl => tpl.OffsetLastWriteTime)
@@ -32,16 +46,48 @@ namespace MoveFileByTimestamp
                     var ymd = group.Key.ToString("yyyyMMdd");
                     Directory.CreateDirectory(ymd);
 
+                    var dstDirPath = Path.Combine(
+                        Path.GetDirectoryName(group.First().FileName),
+                        ymd);
+                    var nFiles = 0;
+                    var sumFileSize = 0L;
                     foreach (var fileName in group.Select(tpl => tpl.FileName))
                     {
                         var dstFilePath = Path.Combine(
-                            Path.GetDirectoryName(fileName),
-                            ymd,
+                            dstDirPath,
                             Path.GetFileName(fileName));
                         if (!File.Exists(dstFilePath))
                         {
                             File.Move(fileName, dstFilePath);
                         }
+                        nFiles++;
+                        sumFileSize += new FileInfo(dstFilePath).Length;
+                    }
+
+                    var zipFilePath = dstDirPath + ".zip";
+                    if (sumFileSize < MaxSumFileSize)
+                    {
+                        if (File.Exists(zipFilePath))
+                        {
+                            File.Delete(zipFilePath);
+                        }
+                        Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Create Zip archive: {zipFilePath} ({nFiles} files: {sumFileSize / 1024.0 / 1024.0:f3} MB) ...");
+                        using (var archive = ZipFile.Open(zipFilePath, ZipArchiveMode.Create))
+                        {
+                            foreach (var fileName in Directory.EnumerateFiles(dstDirPath))
+                            {
+                                archive.CreateEntryFromFile(
+                                    fileName,
+                                    ymd + "/" + Path.GetFileName(fileName),
+                                    CompressionLevel.Optimal);
+                            }
+                        }
+                        var zipFileSize = new FileInfo(zipFilePath).Length;
+                        Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Create Zip file done!: {zipFilePath} ({zipFileSize / 1024.0 / 1024.0:f3} MB, Defrated {(1.0 - (double)zipFileSize / sumFileSize) * 100.0:f2} %)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Ignore to create Zip archive: {zipFilePath} ({nFiles} files: {sumFileSize / 1024.0 / 1024.0:f3} MB)");
                     }
                 });
         }
